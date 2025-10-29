56b85ce356f74a95d7441fc6c693a4479f5486b7
AbdulsemedA <abdulsemedabdelila65@gmail.com>
2025-10-29 10:15:02 +0300

    Refactor URL routing and views to use ViewSets for products and collections, enhancing code simplicity and request handling

--- store/views.py
+++ store/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
 from .serializers import CollectionSerializer, ProductSerializer
-from .models import Collection, Product
+from .models import Collection, OrderItem, Product
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
@@ -13,20 +13,18 @@ class ProductViewSet(ModelViewSet):
     def get_serializer_context(self):
         return { 'request': self.request }
 
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        if product.orderitems.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
             return Response({'error': "Can't delete products as it contains one or more order items."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
+        
+        return super().destroy(request, *args, **kwargs)
+    
 class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count('products')).all()
     serializer_class = CollectionSerializer
 
-    def delete(self, request, pk):
-        collection = get_object_or_404(Collection, pk=pk)
-        if collection.products.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if Collection.objects.count('products') > 0:
             return Response({'error': "Can't delete collection as it contains one or more products."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+        return super().destroy(request, *args, **kwargs)
