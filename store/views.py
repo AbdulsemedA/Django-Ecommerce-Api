461b8c397938797fc4d0ce86b039df7c0fe0ddfa
AbdulsemedA <abdulsemedabdelila65@gmail.com>
2025-10-29 09:59:01 +0300

    Refactor views to use ModelViewSet for Product and Collection, simplifying code and enhancing request handling

--- store/views.py
+++ store/views.py
@@ -1,40 +1,29 @@
 from django.shortcuts import get_object_or_404
 from django.db.models.aggregates import Count
-
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
-
 from .serializers import CollectionSerializer, ProductSerializer
-
 from .models import Collection, Product
 
-class ProductList(ListCreateAPIView):
-    queryset = Product.objects.select_related('collection').all()
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    
+
     def get_serializer_context(self):
         return { 'request': self.request }
 
-class ProductDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    
     def delete(self, request, pk):
         product = get_object_or_404(Product, pk=pk)
         if product.orderitems.count() > 0:
             return Response({'error': "Can't delete products as it contains one or more order items."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-  
-class CollectionList(ListCreateAPIView):
+
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count('products')).all()
     serializer_class = CollectionSerializer
 
-class CollectionDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Collection.objects.annotate(products_count=Count('products'))
-    serializer_class = CollectionSerializer
-                                           
     def delete(self, request, pk):
         collection = get_object_or_404(Collection, pk=pk)
         if collection.products.count() > 0:
