14136fe4e015f56fcba062dff9f649ae2ae690a1
AbdulsemedA <abdulsemedabdelila65@gmail.com>
2025-10-30 10:02:11 +0300

    Re-design the cart and cartitem models

--- store/models.py
+++ store/models.py
@@ -1,11 +1,11 @@
 from django.core.validators import MinValueValidator
 from django.db import models
+from uuid import uuid4
 
 
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
     discount = models.FloatField()
-
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
@@ -17,7 +17,6 @@ class Collection(models.Model):
 
     class Meta:
         ordering = ['title']
-
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -37,7 +36,6 @@ class Product(models.Model):
 
     class Meta:
         ordering = ['title']
-
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
@@ -63,7 +61,6 @@ class Customer(models.Model):
     class Meta:
         ordering = ['first_name', 'last_name']
 
-
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
     PAYMENT_STATUS_COMPLETE = 'C'
@@ -79,13 +76,11 @@ class Order(models.Model):
         max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
-
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='orderitems')
     quantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
-
 
 class Address(models.Model):
     street = models.CharField(max_length=255)
@@ -93,15 +88,17 @@ class Address(models.Model):
     customer = models.ForeignKey(
         Customer, on_delete=models.CASCADE)
 
-
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 
-
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
+
+    class Meta:
+        unique_together = [['cart', 'product']]
 
 class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
